fe97e7f9fd2774db032e7e2a6d63819673dac800
Midori-Funaki <funamoru@gmail.com>
2018-02-18 15:26:23 +0800

    btn function adjusted

--- project1-main.js
+++ project1-main.js
@@ -146,42 +146,101 @@ $().ready(function(){
     $('.portfolio_btn').on("click",function(e){
         let section = $( e.target ).closest( "section" )[0].id;
         switchToPortfolio(`#${section}`);
+
+        $('.portfolio_btn').addClass('portfolio-active');
+        $('.portfolio_btn span').addClass('btn-title-active');
+        $('.skills_btn').removeClass('skills-active');
+        $('.skills_btn span').removeClass('btn-title-active');
+        $('.about_btn').removeClass('about-active');
+        $('.about_btn span').removeClass('btn-title-active');
+        $('.contact_btn').removeClass('contact-active');
+        $('.contact_btn span').removeClass('btn-title-active');
+
     })
 
     $('#menuSkills').on("click",function(){
         switchToSkills('#main_page');
+
+        $('.skills_btn').addClass('skills-active');
+
+        $('.skills_btn span').addClass('btn-title-active');
     })
 
     $('.skills_btn').on("click",function(e){
         let section = $( e.target ).closest( "section" )[0].id;        
         switchToSkills(`#${section}`);
+
+        $('.skills_btn').addClass('skills-active');
+        $('.skills_btn span').addClass('btn-title-active');
+        $('.portfolio_btn').removeClass('portfolio-active');
+        $('.portfolio_btn span').removeClass('btn-title-active');
+        $('.about_btn').removeClass('about-active');
+        $('.about_btn span').removeClass('btn-title-active');
+        $('.contact_btn').removeClass('contact-active');
+        $('.contact_btn span').removeClass('btn-title-active');
+
     })
 
     $('#menuContact').on("click",function(){
         switchToContact('#main_page');
+
+        $('.contact_btn').addClass('contact-active');
+        $('.contact_btn span').addClass('btn-title-active');
     })
 
     $('.contact_btn').on("click",function(e){
         let section = $( e.target ).closest( "section" )[0].id;                
         switchToContact(`#${section}`);
+
+        $('.contact_btn').addClass('contact-active');
+        $('.contact_btn span').addClass('btn-title-active');
+        $('.skills_btn').removeClass('skills-active');
+        $('.skills_btn span').removeClass('btn-title-active');
+        $('.about_btn').removeClass('about-active');
+        $('.about_btn span').removeClass('btn-title-active');
+        $('.portfolio_btn').removeClass('portfolio-active');
+        $('.portfolio_btn span').removeClass('btn-title-active');
+
     })
 
     $('#menuAbout').on("click",function(){
         switchToAbout('#main_page');
+        $('.about_btn').addClass('about-active');
+        $('.about_btn span').addClass('btn-title-active');
     });
 
     $('.about_btn').on("click",function(e){
         let section = $( e.target ).closest( "section" )[0].id;                
         switchToAbout(`#${section}`);
+
+        $('.about_btn').addClass('about-active');
+        $('.about_btn span').addClass('btn-title-active');
+        $('.skills_btn').removeClass('skills-active');
+        $('.skills_btn span').removeClass('btn-title-active');
+        $('.contact_btn').removeClass('contact-active');
+        $('.contact_btn span').removeClass('btn-title-active');
+        $('.portfolio_btn').removeClass('portfolio-active');
+        $('.portfolio_btn span').removeClass('btn-title-active');
+
     })
 
     $('.home_btn').on("click",function(e){
         let section = $( e.target ).closest( "section" )[0].id;
         switchToHome(`#${section}`);
+
+        $('.skills_btn').removeClass('skills-active');
+        $('.skills_btn span').removeClass('btn-title-active');
+        $('.contact_btn').removeClass('contact-active');
+        $('.contact_btn span').removeClass('btn-title-active');
+        $('.portfolio_btn').removeClass('portfolio-active');
+        $('.portfolio_btn span').removeClass('btn-title-active');
+        $('.about_btn').removeClass('about-active');
+        $('.about_btn span').removeClass('btn-title-active');
     })
 
     //Cube transition
     $('#skills_page').ready(function(){
+
         setTimeout(function(){
             $('.progress-html').animate({width:'60%'},"slower");
             $('.progress-css').animate({width:'60%'},"slower");
@@ -191,5 +250,9 @@ $().ready(function(){
             $('.progress-angular').animate({width:'30%'},"slower");
             $('.progress-ionic').animate({width:'25%'},"slow");
         },700);
+    })
+
+    $('#about_page').ready(function(){
+        
     })
 });
